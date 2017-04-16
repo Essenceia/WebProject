99d7e2b394a44e33abf9ec855aad56d8c127fba6
root <root@localhost.localdomain>
2017-04-16 17:35:39 +0200

    possibiliter d'ajout de user + debut de l'arborecence de gestion des donners utilisateur

---
+++ data/filemanger.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 4/16/17
+ * Time: 4:04 PM
+ */
---
+++ log/looger.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 4/16/17
+ * Time: 5:00 PM
+ */
