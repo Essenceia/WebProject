bdf63edf31a83c0a882b52a521b5a9ce50700988
root <root@localhost.localdomain>
2017-04-18 11:01:12 +0200

    commit befor pull

---
+++ data/postcomment.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 4/18/17
+ * Time: 9:33 AM
+ */
---
+++ data/uploader.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 4/17/17
+ * Time: 9:11 PM
+ */
