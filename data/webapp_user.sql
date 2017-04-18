5e06050abbdad4598d8f4637746b64de0b4dc637
essenceia <mexico00>
2017-04-18 17:07:35 +0200

    recuperation des modifications du css

--- data/webapp_user.sql
+++
@@ -1,4 +0,0 @@
-UPDATE webapp.user SET nom = 'Desmazes', mdp = 'kkk', pseudo = 'sn', photoprofil = 'icon', photofond = 'fond' WHERE email = 'desmazes';
-UPDATE webapp.user SET nom = 'Kyp', mdp = 'Cloine', pseudo = 'Cloine', photoprofil = 'icon', photofond = 'fond' WHERE email = 'kiki';
-UPDATE webapp.user SET nom = 'Champalier', mdp = 'ooo', pseudo = 'Cat', photoprofil = 'icon', photofond = 'fond' WHERE email = 'm.champalier';
-UPDATE webapp.user SET nom = 'Tiercelim', mdp = 'pp', pseudo = 'MusicGod', photoprofil = 'icon', photofond = 'fond' WHERE email = 'tiercelin';
