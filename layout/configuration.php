f28db7d52e4ab5c5f8fb058703cd3d0b901bb175
Marine Champalier <marianec@hotmail.fr>
2017-04-20 17:27:54 +0200

    Petite mise en page sympatoche

--- layout/configuration.php
+++ layout/configuration.php
@@ -33,33 +33,7 @@
         
     }
     else if(isset($_POST['photo']) ){
-        /*
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-        // Check if image file is a actual image or fake image
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        alert(pathinfo($target_file,PATHINFO_EXTENSION));
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-        /*
-        // Check file size
-        if ($_FILES["fileToUpload"]["size"] > 500000) {
-            echo "Fichier trop volumineux";
-            $uploadOk = 0;
-        }
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-            echo "Veuillez choisir un fichier JPG, JPEG, PNG ou GIF";
-            $uploadOk = 0;
-        }*/
+        
     }
 
 ?>
